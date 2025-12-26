274e8b289f17a5bdc29f6d8cfedd923b13d4861a
askash003-knSC <askash003@gmail.com>
2025-12-26 20:40:37 +0700

    Add files via upload

---
+++ database/media.js
@@ -0,0 +1,19 @@
+const videos = [
+  "https://www.youtube.com/embed/h5uL_u6hg-U?si=wmHpYJsV6Mk4sF6x",
+  "https://www.youtube.com/embed/Xk3hdD2On6Q?si=UpZh8v0-kZcm0NmC",
+  "https://www.youtube.com/embed/yW-2dd8PXrc?si=dUIImrFOCCJN97Wc",
+  "https://www.youtube.com/embed/qMelPHEwE8U?si=-GiUN_qgsUO5b8j3",
+];
+
+const images = [
+  "title1.png",
+  "title2.png",
+  "title3.png",
+  "title4.png",
+  "title5.png",
+  "title6.png",
+  "title7.png",
+  "title8.png",
+  "title9.png",
+  "title10.png",
+]
